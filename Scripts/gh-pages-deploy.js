933af5373ddedb5e10e86219fd14ba01082b92c5
Sergi B <bernausergi@gmail.com>
2024-02-10 14:30:02 +0100

    Updated gh-pages-deploy.js

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -2,9 +2,11 @@
 
 import {execa} from "execa";
 import fs from "fs";
+import os from "os";
 
 (async () => {
     try {
+        const isWin = os.platform() === "win32";
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");
@@ -15,7 +17,11 @@ import fs from "fs";
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-        await execa("rm", ["-r", folderName]);
+        if (isWin) {
+            await execa("Remove-Item", ["-LiteralPath", folderName, "-Force", "-Recurse"]);
+        } else {
+            await execa("rm", ["-r", folderName]);
+        }
         await execa("git", ["checkout", "-f", "master"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed, check your settings");
